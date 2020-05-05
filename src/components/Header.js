1b97b302a5bada67afc4b88bb3ffc7886e25d99b
Hyunjoon <hkim778@uwo.ca>
2020-05-05 13:16:23 -0400

    initialize

---
+++ src/components/Header.js
@@ -0,0 +1,73 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <span className="icon fa-user"></span>
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>Hyunjoon Kim</h1>
+        <p>
+          Full Stack developer <br/>
+          Software Engineering Graduate of 2020 
+        </p>
+        <p>
+          Template designed by{' '}
+          <a href="https://html5up.net">HTML5 UP</a> and released
+          <br />
+          for free under the{' '}
+          <a href="https://html5up.net/license">Creative Commons</a> license.
+        </p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('intro')
+            }}
+          >
+            Intro
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            Work
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            About
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Contact
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
+}
+
+export default Header
