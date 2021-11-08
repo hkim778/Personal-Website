2bed066ccdd483bb2fa70da906b882b5b3798474
Hyunjoon <hkim778@uwo.ca>
2021-11-08 10:44:45 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import HomePage from "./page/home";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 style={{ fontWeight: 300 }}>
+        Almost before we knew it, we had left t
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
